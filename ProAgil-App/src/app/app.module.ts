a66cf76ef596404c44a5c7b4a8c27504200831a1
Diego Figliero <diegofigliero@gmail.com>
2021-02-16 19:38:16 -0300

    Adicionando Solution

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/Http'
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      EventosComponent
+      EventosComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
